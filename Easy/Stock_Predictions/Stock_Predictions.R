7c8475322b785173d791f8801460e68ec593a197
pvdwijdeven <pvdwijdeven@gmail.com>
2016-05-28 00:54:13 +0200

    update stock exchange v3

--- Easy/Stock_Predictions/Stock_Predictions.R
+++ Easy/Stock_Predictions/Stock_Predictions.R
@@ -2,35 +2,53 @@ inputset <- read.csv("H:/R/Hackerrank/Easy/Stock_Predictions/input.txt",header=F
 #inputset <- read.csv(file("stdin"),header=FALSE,sep=" ")
 
 # 1st attempt: focus on 1st stock: sell if higher than yesterday, buy if lower than yesterday
-# future attempts:
-# 2nd: same for all stocks, pick highest/lowest
-# 3rd: pick percentage
+# 2nd: same for all stocks, pick highest gain/loss
+# 3rd: pick percentage << works worse than 2nd
 # 4th: linear regression on current dataset
+# future attempts:
 # 5th: linear regression on histyory
 # 6th: same for other algorithms
 # 7th: train on test data
 
+
+
 money=inputset[1,1]
-money=as.numeric(as.character(money))
+money=as.numeric(as.character(money))#factorized due to text in same column
 stocks_available=as.numeric(inputset[1,2])
 days_remaining=as.numeric(inputset[1,3])
 Stocks=(data.frame(inputset[2:(stocks_available+1),]))
-Buysell=data.frame(Name=character(),BS=character(),Amount=integer(),stringsAsFactors=FALSE)
+Buysell=data.frame(Name="STOCK",BS="BUY/SELL",Amount=0,stringsAsFactors=FALSE)
 
-Buysell[1,1]=as.character(Stocks[1,1])
-if(as.numeric(Stocks[2,6])>as.numeric(Stocks[1,7])){
+ScanStocks=Stocks
+counter=1
+ScanStocks=cbind(ScanStocks,abs(ScanStocks[,7]-ScanStocks[,6]))
+while (dim(ScanStocks)[1]>0) {
+  
+#search biggest relative difference (max(|today-yesterday|/today))
+focus=which.max(ScanStocks[,8])
+#write.table(ScanStocks[focus,],row.names=F,col.names=F)
+Buysell[counter,1]=as.character(ScanStocks[focus,1])
+if(as.numeric(ScanStocks[focus,6])>as.numeric(ScanStocks[focus,7])){
   #Price dropped, buy
-  Buysell[1,2]='BUY'
-  Buysell[1,3]=floor(money/as.numeric(Stocks[1,7]))
+  Buysell[counter,2]='BUY'
+  Buysell[counter,3]=floor(money/as.numeric(ScanStocks[focus,7]))
+  money=money-Buysell[counter,3]*as.numeric(ScanStocks[focus,7])
   }else{
     #Price raised, sell
-    Buysell[1,2]='SELL'
-    Buysell[1,3]=as.numeric(Stocks[1,2])
+    Buysell[counter,2]='SELL'
+    Buysell[counter,3]=as.numeric(ScanStocks[focus,2])
+
+  }
+ # write.table(Buysell,col.names=F,row.names=F)
+  if (Buysell[counter,3]!=0){counter=counter+1}else{Buysell=Buysell[-counter,]}
+  ScanStocks=ScanStocks[-focus,]
 
 }
-if (Buysell[1,3]==0){
+counter=counter-1
+if (counter==0){
   cat("0\n")
 }else{
 cat("1\n")
-cat(sprintf("%s", Buysell),sep=" ")
+write.table(Buysell,row.names=FALSE,col.names=FALSE,quote=FALSE)
 }
+
