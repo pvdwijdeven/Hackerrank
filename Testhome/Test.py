41d78dc5b3ad12efcbfeeca9abb859c27051b3fe
pvdwijdeven <pvdwijdeven@gmail.com>
2016-07-14 23:40:41 +0200

    update home env

--- Testhome/Test.py
+++ Testhome/Test.py
@@ -1,55 +1,24 @@
-import random
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n,d = map(int,raw_input().split())
+ar=map(int,raw_input().split())
 
-
-# given absolute is minimized when x = median of L (?)
-def minimize_absolute(mylist):
-    return max(top_k(mylist, 1 + len(mylist) / 2, False))
-
-
-def partition(mylist, v):
-    smaller = []
-    bigger = []
-    same = []
-    for val in mylist:
-        if val < v:
-            smaller.append(val)
-        if val == v:
-            same.append(val)
-        if val > v:
-            bigger.append(val)
-    return smaller, same, [v], bigger
-
-
-def top_k(mylist, k, top):
-    v = mylist[random.randrange(len(mylist))]
-    if top:
-        (right, m_duplicates, middle, left) = partition(mylist, v)
-    else:
-        (left, m_duplicates, middle, right) = partition(mylist, v)
-    if len(left) == k:
-        return left
-    if (len(left) < k) and (len(left) + len(m_duplicates) > k):  # can fill list up with duplicates
-        some_list = left + [v] * (k - len(left))
-        return some_list
-    if len(left) > k:
-        return top_k(left, k, top)
-    if len(left) + len(m_duplicates) == k:
-        return left + m_duplicates
-    return left + m_duplicates + top_k(right, k - len(left) - len(m_duplicates), top)
-
-
-# L = [1,2,3,4,5,6,5,2]
-# print minimize_absolute(L)
-
-
-def test_top_k():
-    test_list = [random.randint(0, 100) for _ in xrange(100)]
-    print top_k(test_list, 50, True)
-    print top_k(test_list, 50, False)
-
-    print minimize_absolute(test_list)
-    test_list.sort()
-    print test_list[len(test_list) / 2]
-
-
-test_top_k()
+total=0
+for j in xrange(n-1):
+    foundi=False
+    for i in xrange(j,-1,-1):
+        if ar[i]==ar[j]-d:
+            foundi=True
+            break
+        elif ar[i]<ar[j]-d:
+            break
+    if foundi:
+        foundk=False
+        for k in xrange(j,n):
+            if ar[k]==ar[j]+d:
+                foundk=True
+                break
+            elif ar[k]>ar[j]+d:
+                break
+    if foundi and foundk:
+        total+=1
+print total
