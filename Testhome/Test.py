57ae761f87ca3d1bafb8f86f4e2f8e9f7eea684b
pvdwijdeven <pvdwijdeven@gmail.com>
2016-07-23 08:39:47 +0200

    update home

--- Testhome/Test.py
+++ Testhome/Test.py
@@ -1,96 +1,13 @@
-def four_on_a_row(rows):
-    found = []
-    for x in rows:
-        for y in x:
-            if found == []:
-                found.append(y)
-            else:
-                if found[0] == y:
-                    found.append(y)
-                else:
-                    found = [y]
-            if len(found) >= 4: return True
-    return False
+import sys
+sys.setrecursionlimit(2000) # 10000
 
+def pyth(n):
+    def p(a, b, c):
+        if n < a + b + c: return []
+        return ([[a, b, c]] if a < b else [[b, a, c]]) \
+             + p(a-b-b+c+c, a+a-b+c+c, a+a-b-b+c+c+c) \
+             + p(a+b+b+c+c, a+a+b+c+c, a+a+b+b+c+c+c) \
+             + p(c+c+b+b-a, c+c+b-a-a, c+c+c+b+b-a-a)
+    return p(3, 4, 5)
 
-def checkio(matrix):
-    # replace this for solution
-    columns = [[] for x in xrange(len(matrix[0]))]
-    diag1 = [[] for x in xrange(len(matrix) - 3)]
-    diag2 = [[] for x in xrange(len(matrix) - 3)]
-    for i, x in enumerate(matrix):
-        for j, y in enumerate(matrix):
-            columns[j].append(matrix[i][j])
-    if four_on_a_row(columns):
-        return True
-    if four_on_a_row(matrix):
-        return True
-    for x in xrange(len(matrix)):
-        for y in xrange(len(matrix)):
-            found = []
-            i = x
-            j = y
-            while i <= len(matrix) - 1 and j <= len(matrix) - 1:
-                if found == []:
-                    found.append(matrix[i][j])
-                else:
-                    if found[0] == matrix[i][j]:
-                        found.append(found[0])
-                    else:
-                        found = [matrix[i][j]]
-                i += 1
-                j += 1
-                if len(found) >= 4: return True
-    for x in xrange(len(matrix)):
-        for y in xrange(len(matrix)):
-            found = []
-            i = x
-            j = y
-            while i <= len(matrix) - 1 and j >= 0:
-                if found == []:
-                    found.append(matrix[i][j])
-                else:
-                    if found[0] == matrix[i][j]:
-                        found.append(found[0])
-                    else:
-                        found = [matrix[i][j]]
-                i += 1
-                j -= 1
-                if len(found) >= 4:
-                    print found
-                    return True
-    return False
-
-
-if __name__ == '__main__':
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    #print  checkio([[7,1,1,8,1,1],[1,1,7,3,1,5],[2,3,1,2,5,1],[1,1,1,5,1,4],[4,6,5,1,3,1],[1,1,9,1,2,1]])
-
-
-    # assert checkio([
-    #     [1, 2, 1, 1],
-    #     [1, 1, 4, 1],
-    #     [4, 3, 1, 6],
-    #     [1, 7, 2, 1]
-    # ]) == True, "Vertical"
-    # assert checkio([
-    #     [7, 1, 4, 1],
-    #     [1, 2, 5, 2],
-    #     [3, 4, 1, 3],
-    #     [1, 1, 8, 1]
-    # ]) == False, "Nothing here"
-    assert checkio([
-        [2, 1, 1, 6, 1],
-        [1, 3, 2, 1, 1],
-        [1, 1, 1, 3, 1],
-        [1, 5, 6, 5, 5],
-        [1, 1, 3, 1, 1]
-    ]) == True, "Long Horizontal"
-    # assert checkio([
-    #     [7, 1, 1, 8, 1, 1],
-    #     [1, 1, 7, 3, 1, 5],
-    #     [2, 4, 1, 1, 5, 1],
-    #     [1, 4, 1, 5, 1, 4],
-    #     [4, 6, 5, 1, 3, 1],
-    #     [1, 1, 9, 1, 2, 1]
-    # ]) == True, "Diagonal"
+pt= pyth(5000000)
--- Testhome/test2.py
+++ Testhome/test2.py
@@ -1,58 +1,35 @@
-import math
+import sys
+sys.setrecursionlimit(2000) # 10000
 
+def pyth(n):
+    def p(a, b, c):
+        if n < a + b + c: return []
+        return ([[a, b, c,a+b+c]] if a < b else [[b, a, c,a+b+c]]) \
+             + p(a-b-b+c+c, a+a-b+c+c, a+a-b-b+c+c+c) \
+             + p(a+b+b+c+c, a+a+b+c+c, a+a+b+b+c+c+c) \
+             + p(c+c+b+b-a, c+c+b-a-a, c+c+c+b+b-a-a)
+    return p(3, 4, 5)
 
-def get_list_of_10000_primes():
-    limit = 104729  # this is the 10000th prime
-    if limit % 2 != 0:
-        limit += 1
-    primes_ = [True] * limit
-    primes_[0], primes_[1] = [None] * 2
-    for ind, val in enumerate(primes_):
-        if val is True:
-            # sieve out non-primes by multiples of known primes
-            primes_[ind * 2::ind] = [False] * (((limit - 1) // ind) - 1)
-    return primes_
+if __name__ == '__main__':
+    import sys
+    pt=[]
+    for x in pyth(5000000):
+        pt.append(x)
 
-
-def get_all_primes_to_limit(limit):
-    primes_ = []
-    sieve_list = get_list_of_10000_primes()
-    for i in xrange(limit + 1):
-        if sieve_list[i]:
-            primes_.append(i)
-    return primes_
-
-
-def get_distinct(n):
-    res = (n - 1) ** 2
-    xl = []
-    total=0
-    xls = set()
-    for a in xrange(2,n+1):#int(math.ceil(math.sqrt(n)))+1):
-        for e in xrange(1, n+1):#int(math.log(n) / math.log(a))+1):
-            for b in xrange(2, n+1):
-                if e*b>n or a**e>n: break
-                val=a**(e*b)
-                if val in xl:
-                    print a,e*b, val,e
-                    total+=1
-                else:
-                    xl.append(val)
-    print res - total
-
-
-def get_distinct2(n):
-    xl = []
-    for a in xrange(2, n + 1):
-        for b in xrange(2, n + 1):
-            if a**b in xl:
-                #pass
-                print a,b,a**b
-            else:
-                xl.append(a ** b)
-    print len(xl)
-
-
-n = input()
-get_distinct2(n)
-get_distinct(n)
+    pl=[0 for x in xrange(5000000)]
+    for x in pt:
+        i=1
+        while x[3]*i<5000000:
+            pl[x[3]*i]+=1
+            i+=1
+    ppp=[]
+    maxx=0
+    maxi=0
+    for i,x in enumerate(pl):
+        if x>maxx:
+            maxx=x
+            maxi=i
+        ppp.append(maxi)
+    for _ in xrange(input()):
+        N=input()
+        print ppp[N]
