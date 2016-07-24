ecc5c1f451229e92482bd91686e805272a2ab38d
pvdwijdeven <pvdwijdeven@gmail.com>
2016-07-24 23:12:38 +0200

    update home

--- Testhome/Test.py
+++ Testhome/Test.py
@@ -1,13 +1,30 @@
-import sys
-sys.setrecursionlimit(2000) # 10000
+import math
 
-def pyth(n):
-    def p(a, b, c):
-        if n < a + b + c: return []
-        return ([[a, b, c]] if a < b else [[b, a, c]]) \
-             + p(a-b-b+c+c, a+a-b+c+c, a+a-b-b+c+c+c) \
-             + p(a+b+b+c+c, a+a+b+c+c, a+a+b+b+c+c+c) \
-             + p(c+c+b+b-a, c+c+b-a-a, c+c+c+b+b-a-a)
-    return p(3, 4, 5)
+def test():
+    K = 2
+    n=7
+    d1 = n * (3 * n - 1) / 2 - (n - K) * (3 * (n - K) - 1) / 2
+    d2 = n * (3 * n - 1) / 2 + (n - K) * (3 * (n - K) - 1) / 2
+    print d1
+    print d2
+    # 3x^2-x-2d=0
+    # 1 + math.sqrt(1-4*3*-2*d1)/6
+    try:
+        x1 = 1 + math.sqrt(1 - 4 * 3 * -2 * d1) / 6.0
+    except ValueError:
+        x1=-1
+    try:
+        x2 = 1 - math.sqrt(1 - 4 * 3 * -2 * d1) / 6.0
+    except ValueError:
+        x2=-1
+    try:
+        x3 = 1 + math.sqrt(1 - 4 * 3 * -2 * d2) / 6.0
+    except ValueError:
+        x3=-1
+    try:
+        x4 = 1 - math.sqrt(1 - 4 * 3 * -2 * d2) / 6.0
+    except ValueError:
+        x4=-1
+    print x1,x2,x3,x4
 
-pt= pyth(5000000)
+test()
--- Testhome/test2.py
+++ Testhome/test2.py
@@ -1,35 +1,4 @@
-import sys
-sys.setrecursionlimit(2000) # 10000
+x="12345"
 
-def pyth(n):
-    def p(a, b, c):
-        if n < a + b + c: return []
-        return ([[a, b, c,a+b+c]] if a < b else [[b, a, c,a+b+c]]) \
-             + p(a-b-b+c+c, a+a-b+c+c, a+a-b-b+c+c+c) \
-             + p(a+b+b+c+c, a+a+b+c+c, a+a+b+b+c+c+c) \
-             + p(c+c+b+b-a, c+c+b-a-a, c+c+c+b+b-a-a)
-    return p(3, 4, 5)
-
-if __name__ == '__main__':
-    import sys
-    pt=[]
-    for x in pyth(5000000):
-        pt.append(x)
-
-    pl=[0 for x in xrange(5000000)]
-    for x in pt:
-        i=1
-        while x[3]*i<5000000:
-            pl[x[3]*i]+=1
-            i+=1
-    ppp=[]
-    maxx=0
-    maxi=0
-    for i,x in enumerate(pl):
-        if x>maxx:
-            maxx=x
-            maxi=i
-        ppp.append(maxi)
-    for _ in xrange(input()):
-        N=input()
-        print ppp[N]
+print x[:2]
+print x[-2:]
