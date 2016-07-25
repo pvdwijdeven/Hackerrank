024712b780eb04b09f679997523767921f4f8322
pvdwijdeven <pvdwijdeven@gmail.com>
2016-07-25 23:51:04 +0200

    home update

--- Testhome/Test.py
+++ Testhome/Test.py
@@ -1,30 +1,73 @@
-import math
+def check_perms(n1, n2):
+    return sorted(str(n1)) == sorted(str(n2))
 
-def test():
-    K = 2
-    n=7
-    d1 = n * (3 * n - 1) / 2 - (n - K) * (3 * (n - K) - 1) / 2
-    d2 = n * (3 * n - 1) / 2 + (n - K) * (3 * (n - K) - 1) / 2
-    print d1
-    print d2
-    # 3x^2-x-2d=0
-    # 1 + math.sqrt(1-4*3*-2*d1)/6
-    try:
-        x1 = 1 + math.sqrt(1 - 4 * 3 * -2 * d1) / 6.0
-    except ValueError:
-        x1=-1
-    try:
-        x2 = 1 - math.sqrt(1 - 4 * 3 * -2 * d1) / 6.0
-    except ValueError:
-        x2=-1
-    try:
-        x3 = 1 + math.sqrt(1 - 4 * 3 * -2 * d2) / 6.0
-    except ValueError:
-        x3=-1
-    try:
-        x4 = 1 - math.sqrt(1 - 4 * 3 * -2 * d2) / 6.0
-    except ValueError:
-        x4=-1
-    print x1,x2,x3,x4
 
-test()
+def primes(n):
+    """ Returns  a list of primes < n """
+    sieve = [True] * n
+    for i in xrange(3, int(n ** 0.5) + 1, 2):
+        if sieve[i]:
+            sieve[i * i::2 * i] = [False] * ((n - i * i - 1) / (2 * i) + 1)
+    prime_list_ = [2] + [i for i in xrange(3, n, 2) if sieve[i]]
+    return prime_list_[168:], sieve
+
+
+n, k = map(int, raw_input().split())
+prime_list, sieve_list = primes(1000000)
+
+
+def is_prime(number):
+    try:
+        x = number == 2 or (sieve_list[number] and number % 2 != 0)
+    except IndexError:
+        print number
+        raise IndexError
+    return x
+
+
+def k3():
+    my_primes = [[] for x in xrange(0, 7)]
+    for x in prime_list:
+        my_primes[len(str(x))].append(x)
+    sum = 0
+    for x in xrange(4, 7):
+        for i, p in enumerate(my_primes[x]):
+            if p > n: break
+            if p > 10 ** x / k:
+                break
+            for j in my_primes[x][i + 1:]:
+                if j + j - p < ((10 ** x)):
+                    if is_prime(j + j - p):
+                        if not check_perms(j, p): continue
+                        if check_perms(j + j - p, p):
+                            sum += 1
+                            print str(p) + str(j) + str(j + (j - p))
+                else:
+                    break
+
+
+def k4():
+    my_primes = [[] for x in xrange(0, 7)]
+    for x in prime_list:
+        my_primes[len(str(x))].append(x)
+    for x in xrange(4, 7):
+        for i, p in enumerate(my_primes[x]):
+            if p > n: break
+            if p > 10 ** x / k:
+                break
+            for j in my_primes[x][i + 1:]:
+                if j + (j - p)+(j-p) < ((10 ** x)):
+                    if is_prime(j + j - p):
+                        if not check_perms(j, p): continue
+                        if check_perms(j + j - p, p):
+                            if is_prime(j + (j - p) + (j - p)):
+                                if check_perms(p, j + (j - p) + (j - p)):
+                                    print str(p) + str(j) + str(j + (j - p)) + str(j + (j - p) + (j - p))
+                else:
+                    break
+
+
+if k == 3:
+    k3()
+else:
+    k4()
--- Testhome/test2.py
+++ Testhome/test2.py
@@ -1,4 +1,4 @@
-x="12345"
-
-print x[:2]
-print x[-2:]
+my_primes=[[] for x in xrange(0,6)]
+print my_primes
+print 9999%10**3
+print 9999%10**5
