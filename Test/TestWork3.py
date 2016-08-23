27737ee4230900edc1ab87da954fdddc505b66e3
pvdwijdeven <pvdwijdeven@gmail.com>
2016-08-23 15:30:53 +0200

    monop

--- Test/TestWork3.py
+++ Test/TestWork3.py
@@ -1,22 +1,86 @@
-def checkio(number):
-    pigs = 1
-    tot_pigs=1
-    feed = 1
-    while number > feed:
-        pigs+=1
-        tot_pigs+=pigs
-        feed+=tot_pigs
-        if feed>=number:
-            if feed-number>pigs:
-                return tot_pigs-pigs
-            else:
-                return tot_pigs-(feed-number)
-    return feed
+from random import randint
+
+CH_actions = ['E3', 'JAIL', 'C1', 'GO', 'H2', 'R1', 'NEXT R', 'NEXT R', 'NEXT U', '-3', 'STAY', 'STAY', 'STAY',
+              'STAY', 'STAY', 'STAY']
+
+CC_actions = ['STAY', 'JAIL', 'STAY', 'STAY', 'GO', 'STAY', 'STAY', 'STAY', 'STAY', 'STAY', 'STAY', 'STAY', 'STAY',
+              'STAY', 'STAY', 'STAY']
+
+dict_board = {0: 'GO', 1: 'A1', 2: 'CC1', 3: 'A2', 4: 'T1', 5: 'R1', 6: 'B1', 7: 'CH1', 8: 'B2', 9: 'B3', 10: 'JAIL',
+              11: 'C1', 12: 'U1', 13: 'C2', 14: 'C3', 15: 'R2', 16: 'D1', 17: 'CC2', 18: 'D2', 19: 'D3', 20: 'FP',
+              21: 'E1', 22: 'CH2', 23: 'E2', 24: 'E3', 25: 'R3', 26: 'F1', 27: 'F2', 28: 'U2', 29: 'F3', 30: 'G2J',
+              31: 'G1', 32: 'G2', 33: 'CC3', 34: 'G3', 35: 'R4', 36: 'CH3', 37: 'H1', 38: 'T2', 39: 'H2'}
+
+dict_board_back = {'GO': 0, 'A1': 1, 'CC1': 2, 'A2': 3, 'T1': 4, 'R1': 5, 'B1': 6, 'CH1': 7, 'B2': 8, 'B3': 9,
+                   'JAIL': 10, 'C1': 11, 'U1': 12, 'C2': 13, 'C3': 14, 'R2': 15, 'D1': 16, 'CC2': 17, 'D2': 18,
+                   'D3': 19, 'FP': 20, 'E1': 21, 'CH2': 22, 'E2': 23, 'E3': 24, 'R3': 25, 'F1': 26, 'F2': 27, 'U2': 28,
+                   'F3': 29, 'G2J': 30, 'G1': 31, 'G2': 32, 'CC3': 33, 'G3': 34, 'R4': 35, 'CH3': 36, 'H1': 37,
+                   'T2': 38, 'H2': 39}
 
 
-if __name__ == '__main__':
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    assert checkio(1) == 1, "1st example"
-    assert checkio(2) == 1, "2nd example"
-    assert checkio(5) == 3, "3rd example"
-    assert checkio(10) == 6, "4th example"
+def get_action(current):
+    current = dict_board[current]
+    if current == "CC1" or current == "CC2" or current == "CC3":
+        x = randint(0, 15)
+        action = CC_actions[x]
+    elif current == "CH1" or current == "CH2" or current == "CH3":
+        x = randint(0, 15)
+        action = CH_actions[x]
+    elif current == "G2J":
+        action = 'JAIL'
+    else:
+        action = 'STAY'
+    return action
+
+
+def get_new_pos(action, pos):
+    if action in dict_board_back:
+        return dict_board_back[action]
+    elif action == 'STAY':
+        return pos
+    elif action == 'NEXT R':
+        if pos < 4 or pos > 35:
+            pos = 5
+        elif 5 < pos < 15:
+            pos = 15
+        elif 15 < pos < 25:
+            pos = 25
+        elif 25 < pos < 35:
+            pos = 35
+        return pos
+    elif action == 'NEXT U':
+        if 12 < pos <= 27:
+            return 28
+        else:
+            return 12
+    elif action == "-3":
+        pos -= 3
+        if pos < 0:
+            pos += 40
+        if pos == 33:
+            pos = get_new_pos(get_action(pos), pos)
+        return pos
+    else:
+        exit("unknown action found: %s" % action)
+
+
+def test(max_range):
+    pos = 0
+
+    result = [[0, dict_board[x]] for x in xrange(40)]
+    n, k = map(int, raw_input().split())
+    for x in xrange(max_range):
+        d1 = randint(0, n - 1)
+        d2 = randint(0, n - 1)
+        pos += d1 + d2
+        pos %= 40
+        pos = get_new_pos(get_action(pos), pos)
+        result[pos][0] += 1
+    # print result
+    result.sort(key=lambda z: z[0], reverse=True)
+    my_res = result[:k]
+    for x in my_res:
+        print x[1],
+
+
+test(1000000)
--- Test/TestWork5.py
+++ Test/TestWork5.py
@@ -1,141 +1,69 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from os import listdir
+from os.path import isfile, join
+
+ORIGINAL_PATH = "C:\Kaggle\Original CSV"
+SAMPLE_PATH = "C:\Kaggle\Samples"
 
 
-answer = {(u'Boone Junior, Mark', u'Del Toro, Benicio'): None,
-                   (u'Braine, Richard', u'Coogan, Will'): None,
-                   (u'Byrne, Michael (I)', u'Quinn, Al (I)'): None,
-                   (u'Cartwright, Veronica', u'Edelstein, Lisa'): None,
-                   (u'Curry, Jon (II)', u'Wise, Ray (I)'): None,
-                   (u'Di Benedetto, John', u'Hallgrey, Johnathan'): None,
-                   (u'Hochendoner, Jeff', u'Cross, Kendall'): None,
-                   (u'Izquierdo, Ty', u'Kimball, Donna'): None,
-                   (u'Jace, Michael', u'Snell, Don'): None,
-                   (u'James, Charity', u'Tuerpe, Paul'): None,
-                   (u'Kay, Dominic Scott', u'Cathey, Reg E.'): None,
-                   (u'McCabe, Richard', u'Washington, Denzel'): None,
-                   (u'Reid, Kevin (I)', u'Affleck, Rab'): None,
-                   (u'Reid, R.D.', u'Boston, David (IV)'): None,
-                   (u'Restivo, Steve', u'Preston, Carrie (I)'): None,
-                   (u'Rodriguez, Ramon (II)', u'Mulrooney, Kelsey'): None,
-                   (u'Rooker, Michael (I)', u'Grady, Kevin (I)'): None,
-                   (u'Ruscoe, Alan', u'Thornton, Cooper'): None,
-                   (u'Sloan, Tina', u'Dever, James D.'): None,
-                   (u'Wasserman, Jerry', u'Sizemore, Tom'): None}
-
-test = {(u'Ali, Tony', u'Allen, Woody'): 0.5657,
-        (u'Auberjonois, Rene', u'MacInnes, Angus'): 0.0814,
-        (u'Avery, Shondrella', u'Dorsey, Kimberly (I)'): 0.7837,
-        (u'Bollo, Lou', u'Jeremy, Ron'): 0.4763,
-        (u'Byrne, P.J.', u'Clarke, Larry'): 0.109,
-        (u'Couturier, Sandra-Jessica', u'Jean-Louis, Jimmy'): 0.3649,
-        (u'Crawford, Eve (I)', u'Cutler, Tom'): 0.2052,
-        (u'Flemyng, Jason', u'Newman, Laraine'): 0.139,
-        (u'French, Dawn', u'Smallwood, Tucker'): 0.2979,
-        (u'Gunton, Bob', u'Nagra, Joti'): 0.2136,
-        (u'Hoffman, Jake (I)', u'Shook, Carol'): 0.6073,
-        # (u'Kamiki, Ry\xfbnosuke', u'Thor, Cameron'): 0.3644,
-        (u'Roache, Linus', u'Dreyfuss, Richard'): 0.6731,
-        (u'Sanchez, Phillip (I)', u'Wiest, Dianne'): 0.5083,
-        (u'Sheppard, William Morgan', u'Crook, Mackenzie'): 0.0849,
-        (u'Stan, Sebastian', u'Malahide, Patrick'): 0.2857,
-        (u'Tessiero, Michael A.', u'Molen, Gerald R.'): 0.2056,
-        (u'Thomas, Ken (I)', u'Bell, Jamie (I)'): 0.3941,
-        (u'Thompson, Sophie (I)', u'Foley, Dave (I)'): 0.1095,
-        (u'Tzur, Mira', u'Heston, Charlton'): 0.3642}
-
-import csv
-import heapq
+def get_file_list():
+    files_ = []
+    try:
+        files_ = [f for f in listdir(ORIGINAL_PATH) if isfile(join(ORIGINAL_PATH, f)) and f.endswith(".csv")]
+    except WindowsError:
+        exit("Aborting: %s does not exist, please make sure folder is correct!" % ORIGINAL_PATH)
+    if not files_:
+        exit("Aborting: no .csv files in %s!" % ORIGINAL_PATH)
+    return files_
 
 
-def add_item(H, control, item, priority):
-    if item in control:
-        del_item(H, control, item)
+def investigate_files():
+    files = get_file_list()
+    results = [["filename", "columns", "rows"]]
+    for filename in files:
+        print ("counting lines in %s..." % filename)
+        rows = 0
+        with open(ORIGINAL_PATH + "\\" + filename, 'r') as f:
+            for row in f:
+                rows += 1
+            print ("%s consists of: %i columns and %i rows" % (filename, len(row), rows))
+            results.append([filename, len(row), rows])
+    return results
 
 
-    entry = [priority, item]
-    control[item] = entry
-    heapq.heappush(H, entry)
+def save_results(results):
+    def add_padding(result_):
+        output = ""
+        for element, length in enumerate(max_length):
+            output += '{s:{c}<{n}}'.format(s=result_[element], n=length + 1, c=' ')
+        return output + "\n"
+
+    max_length = []
+    for item in range(len(results[0])):
+        max_length.append(len(str(max(results, key=lambda x: len(str(x[item])))[item])))
+    with open(ORIGINAL_PATH + "\\" + "data_sizes.txt", 'w') as f:
+        for result in results:
+            f.write(add_padding(result))
 
 
-def del_item(H, control, item):
-    entry = control.pop(item)
-    entry[-1] = 'deleted'
+def create_sample_files(sample_size):
+    files = get_file_list()
+    for filename in files:
+        print ("create sample of %s..." % filename)
+        rows = 0
+        results = []
+        with open(ORIGINAL_PATH + "\\" + filename, 'r') as f:
+            while rows <= sample_size:
+                results.append(f.readline())
+                rows += 1
+        try:
+            with open(SAMPLE_PATH + "\\" + str(sample_size) + "sample_" + filename, 'w') as f:
+                for line in results:
+                    f.write(line)
+        except IOError:
+            exit("Aborting: %s does not exist, please create folder first!" % SAMPLE_PATH)
 
 
-def shortest_dist_node(H, control):
-    while H:
-        priority, item = heapq.heappop(H)
-        if item is not 'deleted':
-            del control[item]
-            return item
+#save_results(investigate_files())  # comment this after first use, takes a lot of time!
+create_sample_files(1000)
 
-
-def dijkstra(G, v):
-    H = []  # heap
-    control = {}
-    dist_so_far = {}
-    dist_so_far[v] = 0
-    add_item(H, control, v, dist_so_far[v])
-    final_dist = {}
-    while len(final_dist) < len(G):
-        w = shortest_dist_node(H, control)
-        final_dist[w] = dist_so_far[w]
-        del dist_so_far[w]
-        for x in G[w]:
-            if x not in final_dist:
-                if x not in dist_so_far:
-                    dist_so_far[x] = max(final_dist[w], G[w][x])
-                    add_item(H, control, x, dist_so_far[x])
-                elif final_dist[w] + G[w][x] < dist_so_far[x]:
-                    dist_so_far[x] = max(final_dist[w], G[w][x])
-                    add_item(H, control, x, dist_so_far[x])
-    return final_dist
-
-
-def make_link(G, node1, node2, weight):
-    if node1 not in G:
-        G[node1] = {}
-    (G[node1])[node2] = weight
-    if node2 not in G:
-        G[node2] = {}
-    (G[node2])[node1] = weight
-    return G
-
-
-def read_graph(filename, weighted_movies):
-    # Read an undirected graph in CSV format. Each line is an edge
-    tsv = csv.reader(open(filename), delimiter='\t')
-    G = {}
-    actors = {}
-    movies = {}
-    index = 0
-    for x in tsv:
-        actors[x[0]] = index
-        index += 1
-
-        aux = x[1] + ',' + x[2]
-
-    movies[aux] = 1
-    make_link(G, x[0], aux, weighted_movies[aux])
-
-
-    return G, actors, movies
-
-
-def read_weighted_movies(filename):
-    # Read an undirected graph in CSV format. Each line is an edge
-    tsv = csv.reader(open(filename), delimiter='\t')
-    weighted_movies = {}
-    for x in tsv:
-        weighted_movies[x[0] + ',' + x[1]] = float(x[2])
-    return weighted_movies
-
-
-weighted_movies = read_weighted_movies('imdb-weights.tsv')
-G, actors, movies = read_graph('imdb.tsv', weighted_movies)
-
-print G[G.keys()[0]], G.keys()[0]
-
-for x in sorted(answer.keys()):
-    print x, ':', dijkstra(G, x[0])[x[1]], ','
+print ("done!!!")
